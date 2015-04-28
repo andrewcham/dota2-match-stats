ed5fea492cebac6468a709cf4dc37ee80a1b467c
acham <cham.andrew@gmail.com>
2015-04-27 22:29:24 -0700

    Added production MongoDB string, will try to deploy

--- server/models/Match.js
+++ server/models/Match.js
@@ -1,8 +1,9 @@
 var mongojs = require('mongojs');
-var name = 'Match';
+var localName = 'Match';
+var productionName = 'mongodb://test:test1@ds031632.mongolab.com:31632/matches'
 var collections = ['matches'];
 
-var db = mongojs(name, collections);
+var db = mongojs(productionName, collections);
 db.matches.createIndex({match_id: 1}, {unique: true});
 
 module.exports = db;
